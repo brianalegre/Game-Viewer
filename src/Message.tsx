ccb579c97c58c78f8f475a2c14bd17d9138a3cc5
Brian Alegre <brialegre@yahoo.com>
2023-03-18 21:05:10 -0700

    Created Message Module

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// Using PascalCasing
+function Message() {
+    // JSX: Javascript XML
+    return <h1>Hello World</h1>
+}
+
+// bablejk.io/repl
+// Can write JSX Code, that will convert to JS Code
+
+// Export Module
+export default Message;
